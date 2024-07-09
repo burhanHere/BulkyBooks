08091091892202569d07f41c1acae8124ae56a39
burhanHere <burhanburewala@gmail.com>
2024-07-09 07:38:12 +0500

    gh

--- obj/Debug/net8.0/BulkyBooks.AssemblyInfo.cs
+++ obj/Debug/net8.0/BulkyBooks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBooks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec927828814bbc034e54214b904bd0ae4d42947")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBooks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBooks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
